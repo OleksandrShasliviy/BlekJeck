7e7f74d214853eabdea164e1c5a85ed822f130b9
OleksandrShasliviy <alexxinata@gmail.com>
2022-11-19 12:21:55 +0200

    next etap

--- Game.py
+++ Game.py
@@ -11,6 +11,7 @@ class Game:
         self.dealer = None
         self.all_players_count = 1
         self.deck = Deck()
+        self.max_bet, self.min_bet = 20, 0
 
     @staticmethod
     def ask_starting(message):
@@ -21,6 +22,24 @@ class Game:
             elif choice == 'y':
                 return True
 
+
+    def _launching (self):
+        bot_counts = int(input('Hello, write bot count '))
+        self.all_players_count = bot_counts + 1
+
+        for i in range(bot_counts):
+            # todoo pomenat
+            b = Player.Bot(position=i)
+            self.players.append(b)
+
+            print(b, ' is created ')
+
+            # todoo pomenat
+        self.player = Player.Player(position=bot_counts + 1)
+
+
+    def ask_bet(self):
+
     def start_game(self):
         message = MESSAGES.get("ask_start")
         if not self.ask_starting(message=message):
@@ -29,13 +48,10 @@ class Game:
         bot_counts = int(input('Hello, write bot count '))
         self.all_players_count = bot_counts + 1
 
-        for i in range(bot_counts):
-            #todoo pomenat
-            b = Player.Bot(position=i)
-            self.players.append(b)
-            
-            print(b, ' is created ')
 
-            #todoo pomenat
-        self.player = Player.Player(position=bot_counts + 1)
+        self._launching()
+
+        self.ask_bet()
+
+
 
--- Player.py
+++ Player.py
@@ -4,6 +4,7 @@ class AbstratPlayer(abc.ABC):
     def __init__(self, position):
         self.cards = []
         self.position = position
+        self.bet = 0
 
 
 
@@ -13,8 +14,21 @@ class AbstratPlayer(abc.ABC):
         self.cards.append(card)
         return True
 
+
+    @abc.abstractmethod
+    def change_bet(self):
+        pass
+
+
 class Player(AbstratPlayer):
-    pass
+
+
+    def change_bet(self, max_bet, min_bet):
+        while True:
+            value = int(input('Make your bet: '))
+            if value > max_bet and value < min_bet:
+                self.bet = value
+                break
 
 class Dealer(AbstratPlayer):
     pass
